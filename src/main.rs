02bc8824d25055a45465f38fca32fe85fb9bc13e
Carlo Abelli <carlo@abelli.me>
2019-04-19 17:40:52 -0400

    Switch from clap to structopt

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use clap::{App, Arg};
+use structopt::StructOpt;
 use std::{
     error::Error,
     fs::File,
@@ -7,57 +7,67 @@ use std::{
         Read,
         Write,
     },
+    path::PathBuf,
 };
 
 const CAPACITY: usize = 1024;
 
+#[derive(Debug, StructOpt)]
+#[structopt(
+    about = "Encrypts/decrypts a file using a one-time pad.",
+)]
+struct Opt {
+    #[structopt(
+        parse(from_os_str),
+    )]
+    /// The input file to encrypt/decrypt
+    input: PathBuf,
+
+    #[structopt(
+        short = "o",
+        long = "output",
+        parse(from_os_str),
+    )]
+    /// The output file (default: stdout)
+    output: Option<PathBuf>,
+
+    #[structopt(
+        short = "p",
+        long = "pad",
+        parse(from_os_str),
+    )]
+    /// The pad (key) to use for encryption/decryption
+    pad: PathBuf,
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
-    let matches = App::new("onetime")
-        .version("0.1")
-        .author("Carlo Abelli <carlo@abelli.xyz>")
-        .about("Encrypts/decrypts a file using a one-time pad.")
-        .arg(Arg::with_name("FILE")
-             .required(true)
-             .help("The file to encrypt/decrypt"))
-        .arg(Arg::with_name("PAD")
-             .short("p")
-             .long("pad")
-             .takes_value(true)
-             .value_name("PAD")
-             .required(true)
-             .help("The pad to use for encryption/decryption"))
-        .arg(Arg::with_name("OUTPUT")
-             .short("o")
-             .long("output")
-             .value_name("OUT")
-             .help("The output file (default: stdout)"))
-        .get_matches();
+    let opt = Opt::from_args();
 
-    let mut file = File::open(matches.value_of("FILE").unwrap())?;
-    let mut pad = File::open(matches.value_of("PAD").unwrap())?;
+    let mut input = File::open(opt.input)?;
+    let mut pad = File::open(opt.pad)?;
 
-    if file.metadata()?.len() > pad.metadata()?.len() {
+    if input.metadata()?.len() > pad.metadata()?.len() {
         panic!("file is larger than pad!");
     }
 
-    let mut out: Box<dyn Write> = match matches.value_of("OUTPUT") {
-        Some(name) => Box::new(File::create(name)?),
+    let mut output: Box<dyn Write> = match opt.output {
+        Some(path) => Box::new(File::create(path)?),
         None => Box::new(io::stdout()),
     };
 
-    let mut file_buffer = vec![0u8; CAPACITY];
+    let mut input_buffer = vec![0u8; CAPACITY];
     let mut pad_buffer = vec![0u8; CAPACITY];
     loop {
-        let size = file.read(&mut file_buffer)?;
+        let size = input.read(&mut input_buffer)?;
         if size == 0 {
             break;
         }
         pad.read_exact(&mut pad_buffer[..size])?;
-        let out_buffer: Vec<u8> = pad_buffer.iter()
-            .zip(file_buffer.iter())
-            .map(|(pad_byte, file_byte)| pad_byte ^ file_byte)
+        let output_buffer: Vec<u8> = input_buffer.iter()
+            .zip(pad_buffer.iter())
+            .map(|(input_byte, pad_byte)| input_byte ^ pad_byte)
             .collect();
-        out.write_all(&out_buffer)?;
+        output.write_all(&output_buffer)?;
     }
 
     Ok(())
